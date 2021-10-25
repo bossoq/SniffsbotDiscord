98821f64db1187c2275040c2612026624b7795e2
Kittipos Wajakajornrit <boss.zz@me.com>
2021-10-25 10:42:02 +0700

    Fix Typo in SniffsLotto logs

--- src/lib/PreparedMessage.ts
+++ src/lib/PreparedMessage.ts
@@ -129,7 +129,7 @@ export const preparedLottoDraw = (payload: { [k: string]: any }): SendEmbed => {
     resp = embedMessageBuilder([])
   }
   resp
-    .setTitle(`ประกาศผลรางวัล Sniffscoin ประจำวันที่ ${getThaiDateString()}`)
+    .setTitle(`ประกาศผลรางวัล SniffsLotto ประจำวันที่ ${getThaiDateString()}`)
     .setDescription(
       `เลขที่ออกได้แก่ ${payload.win_number} รางวัลรวม ${payload.payout} Sniffscoin`
     )
